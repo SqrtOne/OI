d37ed1129d826914ae07a0df09ab27f65a9ea1d5
SqrtOne <sqrtone@hotmail.com>
2026-06-05 20:12:35 +0800

    Initial commit

---
+++ Meow.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define Meow int 
+#define meow main
+#define mEoW a
+#define MeOw b
+#define MEow cin
+#define meOW cout
+#define mEow std
+#define meOw +
+#define meoW endl
+#define MEOW return
+#define MeoW 0
+#define MEOw >>
+#define mEOW <<
+#define MeOW {
+#define MEoW }
+#define mEOw ()
+#define MEOW_ ;
+#define meow_ ::
+#define Meow_ ,
+
+Meow meow mEOw MeOW
+	Meow mEoW Meow_ MeOw MEOW_
+	mEow meow_ MEow MEOw mEoW MEOw MeOw MEOW_
+	mEow meow_ meOW mEOW mEoW meOw MeOw mEOW mEow meow_ meoW MEOW_
+	MEOW MeoW MEOW_
+MEoW
---
+++ untitled.cpp
@@ -0,0 +1,13 @@
+// OwO
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define st first
+#define nd second
+
+signed main () {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    
+    return 0;
+}
